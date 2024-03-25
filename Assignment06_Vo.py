32b874471d6e8ca9627746dabe48d4709e9bdb06
Quynh-An <vo.quy@northeastern.edu>
2024-03-25 12:04:46 -0400

    Create LibraryItem and classes Book, DVD, Journal

--- Assignment06_Vo.py
+++ Assignment06_Vo.py
@@ -6,3 +6,66 @@ Created on Thu Mar 21 19:25:04 2024
 @author: tandyllc
 """
 
+# Library Management System
+# Create class LibraryItem as parent class
+# Initialize with title, subject, location in library
+# Create methods check_out() and return_item() and get_details()
+# Book is a child class
+# DVD is a child class
+# Journal is a child class
+# Library Catalog will be a class with all of them with the methods add_item, remove_item, find_item_by_title, check_out, and return_item
+
+class LibraryItem:
+    def __init__(self, title, subject, location):
+        self.title = title
+        self.subject = subject
+        self.location = location
+        
+    def get_details(self):
+        print(f"{self}:")
+        print(f"Title: {self.title}")
+        print(f"Subject: {self.subject}")
+        print(f"Location: {self.location}")
+
+    def check_out(self):
+        pass
+        
+    def return_item(self):
+        pass
+    
+class Book(LibraryItem):
+    inStock = []
+    def __init__(self, title, author, ISBN, subject, location):
+        super().__init__(title, subject, location)
+        self.author = author
+        self.ISBN = ISBN
+        Book.inStock.append(self.title)
+        
+    def check_out(self):
+        Book.inStock.remove(self.title)
+        return print(f"You have checked out {self.title} from the Library.")
+    
+    def return_item(self):
+        Book.inStock.append(self.title)
+        return print(f"You have returned {self.title} to the Library.")
+
+class DVD(LibraryItem):
+    inStock = []
+    def __init__(self, title, subject, location):
+        super().__init__(title, subject, location)
+        self.inStock.append(self.title)
+        
+class Journal(LibraryItem):
+    inStock = []
+    def __init__(self, title, subject, location):
+        super().__init__(title, subject, location)
+        self.inStock.append(self.title)
+    
+# ==========================================
+def main():
+    LittleWomen = Book("Little Women", "Louisa May Alcott", 10000001, "Coming of Age", "Fiction")
+    LittleWomen.check_out()
+    print(Book.inStock)
+    LittleWomen.return_item()
+    
+main()
